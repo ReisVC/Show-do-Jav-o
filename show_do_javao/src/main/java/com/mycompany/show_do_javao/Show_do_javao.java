6ab0ca1f210ff054dd427926cb37069b5170641e
ReisVC <vitor.reisvc@gmail.com>
2025-05-27 14:47:03 -0300

    chore: criado projeto com Maven em Java

---
+++ show_do_javao/src/main/java/com/mycompany/show_do_javao/Show_do_javao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.show_do_javao;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Show_do_javao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
